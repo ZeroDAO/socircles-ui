4241e104ad19792317c99d7616812778fd890f99
DarkingLee <xiuerdwy@gmail.com>
2021-06-11 15:34:32 +0800

    fix i18n

--- src/util/localstorage.js
+++ src/util/localstorage.js
@@ -3,11 +3,12 @@ const localstorage = {
     set(key, value, expired) {
         localStorage.setItem(key, JSON.stringify({
             data: value,
-            expired: expired ? Date.now() + (1000 * 60 * expired) : null
+            expired: expired == 0 ? 0 : Date.now() + (1000 * 60 * expired)
         }));
     },
     fetch(key) {
         const value = JSON.parse(localStorage.getItem(key));
+        console.log('value: ', value);
         if (value) {
             // 设置为 0 时，则不会过期
             if (value.expired != 0 && Date.now() >= value.expired) {
